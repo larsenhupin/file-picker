01bfd725de87b2bae30c02a87c527fecb02144d9
Antoine Girard Dubreuil <antoine.dubreuil91@gmail.com>
2020-09-25 13:06:36 -0400

    add scrollbar

--- code/view.py
+++ code/view.py
@@ -25,16 +25,16 @@ class View():
 		self.programButton.grid(row=10,column=1, pady=100)
 
 	def init_program_window(self):
-		self.pathSrcText = Label(self.frameProgram, text="Source:")
-		self.pathSrcText.grid(row=0, column=0, padx=0, sticky='w')
+		pathSrcText = Label(self.frameProgram, text="Source:")
+		pathSrcText.grid(row=0, column=0, padx=0, sticky='w')
 		self.pathSrcEntryBox = Entry(self.frameProgram, textvariable="5", width=50)
 		self.pathSrcEntryBox.grid(row=0, column=1, columnspan=2, padx=0, pady=0, sticky='e')
 		self.pathSrcEntryBox.configure(state="readonly", readonlybackground="white")
 		self.pathSrcButton = Button(self.frameProgram, text='...', width=5, height=1)
 		self.pathSrcButton.grid(row=0, column=3, padx=(30,0), sticky='e')
 		
-		self.pathDestText = Label(self.frameProgram, text='Destination:')
-		self.pathDestText.grid(row=1, column=0,padx=0, sticky='w')
+		pathDestText = Label(self.frameProgram, text='Destination:')
+		pathDestText.grid(row=1, column=0,padx=0, sticky='w')
 		self.pathDestEntryBox = Entry(self.frameProgram, textvariable='6', width=50)
 		self.pathDestEntryBox.grid(row=1, column=1, columnspan=2, padx=0, pady=0, sticky='w')
 		self.pathDestEntryBox.configure(state="readonly", readonlybackground="white")
@@ -59,13 +59,13 @@ class View():
 
 		# ------------------------------------------------------------------------------------------------
 
-		self.numberRandomText = Label(self.frameProgram, text='Number of files:')
-		self.numberRandomText.grid(row=7, column=2,padx=0,pady=0,sticky='w')
+		numberRandomText = Label(self.frameProgram, text='Number of files:')
+		numberRandomText.grid(row=7, column=2,padx=0,pady=0,sticky='w')
 		self.numberRandomEntry = Entry(self.frameProgram, textvariable="7", width=10, state="disable")
 		self.numberRandomEntry.grid(row=7, column=3,padx=0,pady=0,sticky='e')
 
-		self.extDerouleText = Label(self.frameProgram, text="Extensions :")
-		self.extDerouleText.grid(row=8, column=2, sticky='w', padx=0, pady=0)
+		extDerouleText = Label(self.frameProgram, text="Extensions :")
+		extDerouleText.grid(row=8, column=2, sticky='w', padx=0, pady=0)
 
 		self.extComboBox = ttk.Combobox(self.frameProgram, width=5, state='disable')
 		self.extComboBox.grid(row=8, column=3, sticky='e')
@@ -76,11 +76,17 @@ class View():
 		self.infoText.configure(state='disable')
 
 		self.infoListbox = Listbox(self.frameProgram, width=25, height=10)
+
+		scrollbar = Scrollbar(self.frameProgram, orient="vertical")
+
+		scrollbar.config(command=self.infoListbox.yview)
+		scrollbar.grid(row=5,column=1, rowspan=6,sticky=N+S+E)
+		self.infoListbox.config(yscrollcommand=scrollbar.set)
 		self.infoListbox.grid(row=5, column=0, rowspan=6, columnspan=2, sticky='w')
 		self.infoListbox.configure(bd=1, relief="sunken", highlightthickness=0, selectbackground="white", selectforeground="black", activestyle ="none")
 
 		self.pickButton = Button(self.frameProgram,text='Pick', width=10)
-		self.pickButton.grid(row=9, column=2, columnspan=2, sticky='e')
+		self.pickButton.grid(row=9, column=1, columnspan=2, sticky='e')
 
 	def getIsAllCheckbox(self):
 		self.isRandom.set(0)
